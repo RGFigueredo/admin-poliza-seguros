76c21062b95f8690071590f55ba0eddb9e89a53b
Jose Suarez <jozhee@live.com>
2017-02-03 21:10:03 -0400

    borrado archivos inutiles

--- core/modulos/eliminar.php
+++ core/modulos/eliminar.php
@@ -1,1 +1,34 @@
+<?PHP
 
+if (!defined('SRCP')) {
+    die('Logged Hacking attempt!');
+}
+
+// funcion borrar cualquiera! - inseguro pero funciona.
+function Query( $get_var ) 
+{
+    switch($get_var)
+    {
+        case 'corredor':
+            $tbl = 'corredores';
+            break;
+        case 'asegurados':
+            $tbl = 'corredores';
+            break;
+        case 'beneficiaros':
+            $tbl = 'corredores';
+            break;
+        case 'polizas':
+            $tbl = 'corredores';
+            break;
+        case 'planes':
+            $tbl = 'corredores';
+            break;
+        case 'usuarios':
+            $tbl = 'corredores';
+            break;
+
+    }
+
+    $sql = "SELECT * FROM $tbl";
+}
