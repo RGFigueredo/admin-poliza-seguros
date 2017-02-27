2467ff018d61bf7310344375a5db8b41bcf6b78e
Jose Suarez Stronghold <Jose Suarez Stronghold>
2017-02-27 11:24:47 -0400

    fixed db transactions

--- core/inc/reg_reembolsos.php
+++ core/inc/reg_reembolsos.php
@@ -1,84 +1,54 @@
 <?php
 
 if (!defined('SRCP')) {
-    die('Logged Hacking attempt!');
+	
+	die('Logged Hacking attempt!');
+	
 }
-    //comienzo del registro de los corredor.
-    if (!empty($_POST)) {
 
-        $query = '  SELECT 1
+//comienzo del registro de los corredor.
+if (!empty($_POST)) {
+	$db->beginTransaction();
+	
+	$query = '  SELECT 1
             		FROM reembolsos
             		WHERE asegurado_cedula = :asegurado_cedula
         		  ';
-        $query_params = array(':asegurado_cedula' => $_POST['asegurado_cedula']);
-        try {
-            $stmt = $db->prepare($query);
-            $result = $stmt->execute($query_params);
-        } catch (PDOException $ex) {
-            /*
+	
+	$query_params = array(':asegurado_cedula' => $_POST['asegurado_cedula']);
+	
+	try {
+		
+		$stmt = $db->prepare($query);
+		
+		$result = $stmt->execute($query_params);
+		
+	}
+	catch (PDOException $ex) {
+		
+		
+		/*
         	  TODO: Aqui de igual forma cambiaremos a un modal
         	 */
-            die('Fallamos al hacer la busqueda: '.$ex->getMessage());
-        }
-        $row = $stmt->fetch();
-        if ($row) {
-            echo "<div class='panel-body'>
+		
+		die('Fallamos al hacer la busqueda: '.$ex->getMessage());
+		
+	}
+	
+	$row = $stmt->fetch();
+	
+	if ($row) {
+		
+		echo "<div class='panel-body'>
                 <div class='alert alert-warning alert-dismissable'>
                 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
                 Error: La cédula ya existe.</div>
             </div>";
-        }
-        $query = 'SELECT 1
-            	  FROM reembolsos
-            	  WHERE asegurado_cedula = :asegurado_cedula
-        		 ';
-        $query_params = array(
-            ':asegurado_cedula' => $_POST['asegurado_cedula'],
-        );
-        try {
-            $stmt = $db->prepare($query);
-            $result = $stmt->execute($query_params);
-        } catch (PDOException $ex) {
-            die('Fallamos al revisar el email.: '.$ex->getMessage());
-        }
-        $row = $stmt->fetch();
-        if ($row) {
-            echo "<div class='panel-body'>
-                <div class='alert alert-warning alert-dismissable'>
-                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-                Error: El correo ya esta en uso.</div>
-            </div>";
-        }
-
-        
-        $query = 'UPDATE gastos SET reembolso = :reembolso
-                WHERE codigo = :gastos_codigo
-                ';
-        $query_params = array(           
-            ':reembolso' => $_POST['reembolso']
-            );
-          try {
-            $stmt = $db->prepare($query);
-            $result = $stmt->execute($query_params);
-                    
-                    
-        } catch (PDOException $ex) {
-            // Si tenemos problemas para ejecutar la consulta imprimimos el error
-            echo "<div class='panel-body'>
-                     <div class='alert alert-warning alert-dismissable'>
-                        <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-                        Tenemos problemas al ejecutar la consulta :c El error es el siguiente:
-					</div>
-				  </div>".$ex->getMessage();
-        }
- 
-        
-        
-        
-        
-        
-        /// Si todo pasa enviamos los datos a la base de datos mediante PDO para evitar Inyecciones SQL
-        $query = '	INSERT INTO reembolsos (
+		
+	}
+	
+	///	Si todo pasa enviamos los datos a la base de datos mediante PDO para evitar Inyecciones SQL
+	$query2 = '	INSERT INTO reembolsos (
 				                gastos_codigo,
                                 asegurado_cedula,
                                 monto, 
@@ -93,46 +63,59 @@ if (!defined('SRCP')) {
                                 :estatus
 				            )
         		';
-        $query_params = array(
-            ':gastos_codigo' => $_POST['gastos_codigo'],
-            ':asegurado_cedula' => $_POST['asegurado_cedula'],
-            ':monto' => $_POST['monto'],
-            ':fecha' => $_POST['fecha'],
-            ':estatus' => $_POST['estatus']            
-            );
-        try {
-            $stmt = $db->prepare($query);
-            $result = $stmt->execute($query_params);        
-        } catch (PDOException $ex) {
-            // Si tenemos problemas para ejecutar la consulta imprimimos el error
-            echo "<div class='panel-body'>
+	
+	$query_params = array(
+	':gastos_codigo' => $_POST['gastos_codigo'],
+	':asegurado_cedula' => $_POST['asegurado_cedula'],
+	':monto' => $_POST['monto'],
+	':fecha' => $_POST['fecha'],
+	':estatus' => $_POST['estatus']            
+	);
+	
+	try {
+		
+		$stmt = $db->prepare($query2);
+		
+		$result = $stmt->execute($query_params);
+		
+	}
+	catch (PDOException $ex) {
+		
+		// 		Si tenemos problemas para ejecutar la consulta imprimimos el error
+		echo "<div class='panel-body'>
                      <div class='alert alert-warning alert-dismissable'>
                         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
                         Tenemos problemas al ejecutar la consulta :c El error es el siguiente:
 					</div>
 				  </div>".$ex->getMessage();
-        }
-        //AQUI TIENE QUE ACTUALIZAR GASTOS
-        //SE DEBE CAMBIAR LA TABLA reembolso("Pendiente") a reembolso("Pagado");
-        
-        
-        
+		
+	}
+$db->commit();	
+	
+	
+	header('Location: index.php?do=listareembolsos');
+	
+}
 
-        header('Location: index.php?do=listareembolsos');
-    }
 
-    if (isset($_GET['accion'])) {
-
-    //check the action
-    switch ($_GET['accion']) {
-        case 'error':
-            echo "<div class='panel-body'>
+if (isset($_GET['accion'])) {
+	
+	
+	//c	heck the action
+	switch ($_GET['accion']) {
+		
+		case 'error':
+		echo "<div class='panel-body'>
                     <div class='alert alert-success alert-dismissable'>
                       <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
                       Hay varios errores en tu registro.
                       Si necesitas ayuda puedes hacer clic al botón de Ayuda en el fondo de la página.
 					</div>
 				</div>";
-            break;
-    }
-    }
+		
+		break;
+		
+	}
+	
+}
+
