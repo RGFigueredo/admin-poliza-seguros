07978445b2248e23f60ae1676e287c582dbafa76
Erick Suarez <erick.suarez@outlook.com>
2017-03-09 07:33:52 -0400

    Add files via upload

--- assets/js/validar.js
+++ assets/js/validar.js
@@ -1,41 +1,109 @@
-$(document).ready(function() {
-    $('.input-group input[required], .input-group textarea[required], .input-group select[required]').on('keyup change', function() {
-		var $form = $(this).closest('form'),
-            $group = $(this).closest('.input-group'),
-			$addon = $group.find('.input-group-addon'),
-			$icon = $addon.find('span'),
-			state = false;
-            
-    	if (!$group.data('validate')) {
-			state = $(this).val() ? true : false;
-		}else if ($group.data('validate') == "email") {
-			state = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test($(this).val())
-		}else if($group.data('validate') == 'phone') {
-			state = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/.test($(this).val())
-		}else if ($group.data('validate') == "length") {
-			state = $(this).val().length >= $group.data('length') ? true : false;
-		}else if ($group.data('validate') == "number") {
-			state = !isNaN(parseFloat($(this).val())) && isFinite($(this).val());
-		}
+<script type="text/javascript">
+//datepicker plugin
+		//link
+		$('.date-picker').datepicker({
+				autoclose: true,
+				todayHighlight: true
+			})
+			//Mostrar el datepicker al hacer click en el icono
+			.next().on(ace.click_event, function() {
+				$(this).prev().focus();
+			});
+		//to translate the daterange picker, please copy the "examples/daterange-fr.js" contents here before initialization
+		$('input[name=date-range-picker]').daterangepicker({
+				'applyClass': 'btn-sm btn-success',
+				'cancelClass': 'btn-sm btn-default',
+				locale: {
+					applyLabel: 'Apply',
+					cancelLabel: 'Cancel',
+				}
+			})
+			.prev().on(ace.click_event, function() {
+				$(this).next().focus();
+			});
+      // otro
+            $('#registrationForm').bootstrapValidator({
+            	 feedbackIcons: {
+            		 valid: 'glyphicon glyphicon-ok',
+            		 invalid: 'glyphicon glyphicon-remove',
+            		 validating: 'glyphicon glyphicon-refresh'
+            	 },
+            	 fields: {
+            		 nombre: {
+            			 validators: {
+            				 notEmpty: {
+            					 message: 'El nombre es requerido'
+            				 }
+            			 }
+            		 },
+            		 apellido: {
+            			 validators: {
+            				 notEmpty: {
+            					 message: 'El apellido es requerido'
+            				 }
+            			 }
+            		 },
+            		 email: {
+            			 validators: {
+            				 notEmpty: {
+            					 message: 'El correo es requerido y no puede ser vacio'
+            				 },
+            				 emailAddress: {
+            					 message: 'El correo electronico no es valido'
+            				 }
+            			 }
+            		 },
+            		 password: {
+            			 validators: {
+            				 notEmpty: {
+            					 message: 'El password es requerido y no puede ser vacio'
+            				 },
+            				 stringLength: {
+            					 min: 8,
+            					 message: 'El password debe contener al menos 8 caracteres'
+            				 }
+            			 }
+            		 },
+            		 datetimepicker: {
+            			 validators: {
+            				 notEmpty: {
+            					 message: 'La fecha de nacimiento es requerida y no puede ser vacia'
+            				 },
+            				 date: {
+            					 format: 'YYYY-MM-DD',
+            					 message: 'La fecha de nacimiento no es valida'
+            				 }
+            			 }
+            		 },
+            		 sexo: {
+            			 validators: {
+            				 notEmpty: {
+            					 message: 'El sexo es requerido'
+            				 }
+            			 }
+            		 },
+            		 telefono: {
+            			 message: 'El teléfono no es valido',
+            			 validators: {
+            				 notEmpty: {
+            					 message: 'El teléfono es requerido y no puede ser vacio'
+            				 },
+            				 regexp: {
+            					 regexp: /^[0-9]+$/,
+            					 message: 'El teléfono solo puede contener números'
+            				 }
+            			 }
+            		 },
+            		 telefono_cel: {
+            			 message: 'El teléfono no es valido',
+            			 validators: {
+            				 regexp: {
+            					 regexp: /^[0-9]+$/,
+            					 message: 'El teléfono solo puede contener números'
+            				 }
+            			 }
+            		 },
 
-		if (state) {
-				$addon.removeClass('danger');
-				$addon.addClass('success');
-				$icon.attr('class', 'glyphicon glyphicon-ok');
-		}else{
-				$addon.removeClass('success');
-				$addon.addClass('danger');
-				$icon.attr('class', 'glyphicon glyphicon-remove');
-		}
-        
-        if ($form.find('.input-group-addon.danger').length == 0) {
-            $form.find('[type="submit"]').prop('disabled', false);
-        }else{
-            $form.find('[type="submit"]').prop('disabled', true);
-        }
-	});
-    
-    $('.input-group input[required], .input-group textarea[required], .input-group select[required]').trigger('change');
-    
-    
-});
+            	 }
+            });
+              </script>
